319f546ef17a48fc340ae2c5f54ff8e60fd4c917
Rody RATOVONAR <rratovon@e1r3p7.42.fr>
2017-09-20 19:16:36 +0200

    Ajout de d05

---
+++ kek/ex04/ft_strncpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rratovon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/18 08:57:56 by rratovon          #+#    #+#             */
+/*   Updated: 2017/09/18 09:37:32 by rratovon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int index;
+
+	index = 0;
+	while ((src[index] != '\0') && (index < n))
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	if (index == n)
+		dest[index] = '\0';
+	return (dest);
+}
